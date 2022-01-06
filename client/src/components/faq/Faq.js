2bb1989c61fb1e98e72e55ad2ad689911770d210
Priya2123 <aggarwalpriya1121@gmail.com>
2022-01-06 19:16:05 +0530

    wip

--- client/src/components/faq/Faq.js
+++ client/src/components/faq/Faq.js
@@ -9,6 +9,7 @@ import {
 import GroupWorkIcon from "@mui/icons-material/GroupWork";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import DownloadIcon from "@mui/icons-material/Download";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
 
 const Faq = () => {
   return (
@@ -89,6 +90,55 @@ const Faq = () => {
           <Grid item lg={8} md={8} justify="center">
             <Typography variant="h5" style={{ marginBottom: "0.5%" }}>
               <DownloadIcon /> Download
+            </Typography>
+            <Accordion style={{ backgroundColor: "purple" }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon style={{ color: "lavender" }} />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography style={{ color: "lavender" }}>
+                  Accordion 1
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography style={{ color: "lavender" }}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion style={{ backgroundColor: "purple" }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon style={{ color: "lavender" }} />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography style={{ color: "lavender" }}>
+                  Accordion 1
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography style={{ color: "lavender" }}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          lg={12}
+          md={12}
+          justifyContent="center"
+          style={{ paddingTop: "2%" }}
+        >
+          <Grid item lg={8} md={8} justify="center">
+            <Typography variant="h5" style={{ marginBottom: "0.5%" }}>
+              <FileUploadIcon /> Upload
             </Typography>
             <Accordion style={{ backgroundColor: "purple" }}>
               <AccordionSummary
