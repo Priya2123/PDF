98a72322e82f81f4d0d587fa084625f27ee9ed32
Priya2123 <aggarwalpriya1121@gmail.com>
2022-01-11 01:40:15 +0530

    wip

--- client/src/components/tabs/Ask.js
+++ client/src/components/tabs/Ask.js
@@ -52,9 +52,54 @@ const Ask = () => {
           />
         </Grid>
       </Grid>
-      <Grid container lg={12} md={12} style={{ margin: "4% 0 3% 0" }}></Grid>
+      <Grid container lg={12} md={12} style={{ margin: "7% 0 1% 0" }}>
+        <Grid item>
+          <Typography variant="h6">Questions you asked.</Typography>
+        </Grid>
+      </Grid>
       <Grid container lg={12} md={12} justify="center">
         <Grid item lg={10} md={10} justify="center">
+          <Paper elevation={4} style={{ padding: "1%", marginBottom: "2%" }}>
+            <Grid container lg={12} md={12}>
+              <Grid item>
+                {/* <Avatar>P</Avatar> */}
+                <Avatar alt="" src={rishi} />
+              </Grid>
+              <Grid item lg={10} md={10}>
+                <Typography style={{ paddingLeft: "3%" }} variant="body2">
+                  Spoiderman
+                  <br />
+                  <Typography variant="caption" style={{ color: "gray" }}>
+                    @Thapar Institute
+                  </Typography>
+                </Typography>
+              </Grid>
+              <Grid item lg={1} md={1}>
+                <Typography variant="caption">2 days ago</Typography>
+              </Grid>
+            </Grid>
+            <Grid container style={{ padding: "1% 0 0 1%" }}>
+              {/* <Grid item></Grid> */}
+              <Grid item>
+                <Typography variant="h6">Anda pehle aaya ya murgi?</Typography>
+              </Grid>
+            </Grid>
+            <Grid container style={{ padding: "0.2% 0 0 1%" }}>
+              {/* <Grid item></Grid> */}
+              <Grid item>
+                <Typography variant="body1">gadhaaaaaa it is</Typography>
+              </Grid>
+            </Grid>
+            <Grid container style={{ padding: "0.3% 0 0 1%" }}>
+              <Grid item>
+                <ForumIcon
+                  fontSize="small"
+                  style={{ paddingTop: "20%", cursor: "pointer" }}
+                />
+                <Typography variant="caption">7</Typography>
+              </Grid>
+            </Grid>
+          </Paper>
           <Paper elevation={4} style={{ padding: "1%", marginBottom: "2%" }}>
             <Grid container lg={12} md={12}>
               <Grid item>
