1af4c7f8ef73cec445a5c6027860d1f48dcf04b7
Priya2123 <aggarwalpriya1121@gmail.com>
2022-01-11 01:28:06 +0530

    wip

--- client/src/components/tabs/Ask.js
+++ client/src/components/tabs/Ask.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Chip, TextField } from "@mui/material";
 
 const Ask = () => {
   return (
     <Grid container lg={12} md={12} justify="center">
       <Grid item lg={8} md={8} justify="center">
-        hi
+        <Typography variant="body1">Add your question.</Typography>
+        {/* <input
+          label="Question"
+          placeholder="Question"
+          style={{
+            width: "60vw",
+            borderRadius: "20px",
+            // minHeight: "6vw",
+            marginTop: "1%",
+          }}
+        /> */}
+        <TextField
+          multiline={true}
+          rows={5}
+          name="Question"
+          label="Question"
+          placeholder="Please enter your question."
+          autoComplete="on"
+          style={{ width: "100%", marginTop: "1%" }}
+          variant="outlined"
+        />
       </Grid>
     </Grid>
   );
