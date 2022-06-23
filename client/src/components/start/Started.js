6084a47b636c810e7c2db69c90242b7f4b66af13
Priya2123 <aggarwalpriya1121@gmail.com>
2022-06-24 01:18:52 +0530

    WIP

--- client/src/components/start/Started.js
+++ client/src/components/start/Started.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Nav from "../nav/Nav";
 import { Grid, Typography, Button } from "@material-ui/core";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
@@ -13,19 +13,17 @@ import { faWindowRestore } from "@fortawesome/free-solid-svg-icons";
 const Started = () => {
   const classes = useStyles();
 
+  let [user, setUser] = useState("None");
 
-  let[user, setUser] = useState('None')
-
-  useEffect(() =>{
-    getUser()
-  }, [])
+  useEffect(() => {
+    getUser();
+  }, []);
 
   let getUser = async () => {
-    let response = await fetch('http://127.0.0.1:8000/login/')
-    let data = await response.json()
-    setUser(data)
-  }
-
+    let response = await fetch("https://pdffinderapi.herokuapp.com/login/");
+    let data = await response.json();
+    setUser(data);
+  };
 
   const defaultOptions = {
     loop: true,
@@ -35,110 +33,115 @@ const Started = () => {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
-  if(user=='None')
-  {
-  return (
-    <>
-      <Nav />
-      <Parallax
-        pages={2}
-        style={{ top: "0", left: "0", backgroundColor: "#461257" }}
-      >
-        <ParallaxLayer
-          offset={0}
-          speed={2.5}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#461257",
-          }}
+  if (user == "None") {
+    return (
+      <>
+        <Nav />
+        <Parallax
+          pages={2}
+          style={{ top: "0", left: "0", backgroundColor: "#461257" }}
         >
-          <Grid container lg={12} md={12} className={classes.row}>
-            <Grid item lg={12} md={12}>
-              <Grid container justify="center">
-                <Grid item lg={5} md={5}>
-                  <Grid
-                    container
-                    justify="center"
-                    style={{ marginTop: "10%" }}
-                    // className={classes.row}
-                  >
-                    <Grid item lg={11} md={11}>
-                      <Grid
-                        container
-                        justify="center"
-                        style={{ textAlign: "center" }}
-                      >
-                        <Grid item>
-                          <Typography variant="h4" className={classes.heading}>
-                            <span style={{ color: "#61dafb" }}> PDF</span>{" "}
-                            <Typewriter
-                              options={{
-                                strings: ["FINDER", "DOWNLOADER", "UPLOADER"],
-                                autoStart: true,
-                                loop: true,
-                                deleteSpeed: 50,
-                              }}
-                            />
-                          </Typography>
-                        </Grid>
-                        <Grid item sm={11} xs={11}>
-                          <Typography
-                            variant="subtitle1"
-                            className={classes.body}
-                          >
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore
-                            eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia
-                            deserunt mollit anim id est laborum.
-                          </Typography>
-                          <Link to="/login">
-                            {" "}
-                            <Button className={classes.btn}>Sign Up</Button>
-                          </Link>
+          <ParallaxLayer
+            offset={0}
+            speed={2.5}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#461257",
+            }}
+          >
+            <Grid container lg={12} md={12} className={classes.row}>
+              <Grid item lg={12} md={12}>
+                <Grid container justify="center">
+                  <Grid item lg={5} md={5}>
+                    <Grid
+                      container
+                      justify="center"
+                      style={{ marginTop: "10%" }}
+                      // className={classes.row}
+                    >
+                      <Grid item lg={11} md={11}>
+                        <Grid
+                          container
+                          justify="center"
+                          style={{ textAlign: "center" }}
+                        >
+                          <Grid item>
+                            <Typography
+                              variant="h4"
+                              className={classes.heading}
+                            >
+                              <span style={{ color: "#61dafb" }}> PDF</span>{" "}
+                              <Typewriter
+                                options={{
+                                  strings: ["FINDER", "DOWNLOADER", "UPLOADER"],
+                                  autoStart: true,
+                                  loop: true,
+                                  deleteSpeed: 50,
+                                }}
+                              />
+                            </Typography>
+                          </Grid>
+                          <Grid item sm={11} xs={11}>
+                            <Typography
+                              variant="subtitle1"
+                              className={classes.body}
+                            >
+                              Lorem ipsum dolor sit amet, consectetur adipiscing
+                              elit, sed do eiusmod tempor incididunt ut labore
+                              et dolore magna aliqua. Ut enim ad minim veniam,
+                              quis nostrud exercitation ullamco laboris nisi ut
+                              aliquip ex ea commodo consequat. Duis aute irure
+                              dolor in reprehenderit in voluptate velit esse
+                              cillum dolore eu fugiat nulla pariatur. Excepteur
+                              sint occaecat cupidatat non proident, sunt in
+                              culpa qui officia deserunt mollit anim id est
+                              laborum.
+                            </Typography>
+                            <Link to="/login">
+                              {" "}
+                              <Button className={classes.btn}>Sign Up</Button>
+                            </Link>
+                          </Grid>
                         </Grid>
                       </Grid>
                     </Grid>
                   </Grid>
-                </Grid>
-                <Grid item lg={6} md={6}>
-                  <Lottie className={classes.lottie} options={defaultOptions} />
+                  <Grid item lg={6} md={6}>
+                    <Lottie
+                      className={classes.lottie}
+                      options={defaultOptions}
+                    />
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </ParallaxLayer>
+          </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={1}
-          speed={2}
-          style={{ backgroundColor: "#593876" }}
-        />
+          <ParallaxLayer
+            offset={1}
+            speed={2}
+            style={{ backgroundColor: "#593876" }}
+          />
 
-        <ParallaxLayer
-          offset={1}
-          speed={0.5}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            color: "white",
-          }}
-        >
-          <Contact />
-        </ParallaxLayer>
-      </Parallax>
-    </>
-  );
-        }
-  else{
-   window.location.replace('/landing/')
+          <ParallaxLayer
+            offset={1}
+            speed={0.5}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "white",
+            }}
+          >
+            <Contact />
+          </ParallaxLayer>
+        </Parallax>
+      </>
+    );
+  } else {
+    window.location.replace("/landing/");
   }
 };
 
