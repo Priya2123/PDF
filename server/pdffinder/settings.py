c8b099bca2a3e878519b4a7f4f67f6c4974694a3
RishiKr3101 <rishikrofficial@gmail.com>
2022-06-23 14:34:37 +0530

    yes

--- server/pdffinder/settings.py
+++ server/pdffinder/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%hfs$euswcodw!1ejw1(c!d+t(h525dch1j0rlc4=63xfq5rad'
+#SECRET_KEY = '%hfs$euswcodw!1ejw1(c!d+t(h525dch1j0rlc4=63xfq5rad'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
